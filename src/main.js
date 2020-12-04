a427b723ada003e70efa9309405241d4bac243fa
Alex Arriaga <alex.arriaga@webtraining.world>
2020-12-03 20:53:38 -0800

    Add StencilJS component

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import { applyPolyfills, defineCustomElements } from '@alex-arriaga/stencil-sg-virtual-2020/loader';
+
 Vue.config.productionTip = false
+
+// Tell Vue to ignore all components defined in the test-components
+// package. The regex assumes all components names are prefixed
+// 'sg'
+Vue.config.ignoredElements = [/sg-\w*/];
+
+// Bind the custom elements to the window object
+applyPolyfills().then(() => {
+  defineCustomElements();
+});
 
 new Vue({
   render: h => h(App),
